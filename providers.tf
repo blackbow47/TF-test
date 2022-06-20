5c53a6e2f9a127f0307dc74c53cbf39ea66c486f
SCT\pankajg <74521528+blackbow47@users.noreply.github.com>
2022-06-21 00:18:45 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws"{
 region = "us-east-1"
-access_key = "ASIAW5HAFSDE7OXVHHFY"
-secret_key = "7UxgOyIngBpnXw8y3vlPhcKiXoHPNwgQQO5FMOHF"
+access_key = "ASIAW5HAFSDE6XBOLRHI"
+secret_key = "R222q/JuIGqVYoHqZyNVa2MHchxKWA86GX5UNZQQ"
 }
