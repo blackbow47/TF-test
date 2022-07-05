58862c45074414d1973fd0ace0d3e76dc4ab274b
SCT\pankajg <74521528+blackbow47@users.noreply.github.com>
2022-07-05 13:28:40 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
-
+# this is a test
   tags = {
     Name = "VPC-demo-Jen"
     Purpose = "terrafrom using Jenkins"
